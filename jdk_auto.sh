17fcac92c4e83b65c516e472e4f99f90e861ecb7
yds1996 <41829924+yds1996@users.noreply.github.com>
2019-12-12 12:32:00 +0800

    Update jdk_auto.sh

--- jdk_auto.sh
+++ jdk_auto.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #jdk auto install
 echo "=================================="
-echo "===JDK Auto_Install for CentOS===="
+echo "===JDK Auto_Install for Linux ===="
 echo "=================================="
 
 DIR="/usr/java"
-URL="http://qq008.cn/jdk-1.8.0_231.tar.gz"
-FILE="jdk-1.8.0_231.tar.gz"
+FILE_ZIP="jdk-8u231-linux-x64.tar.gz"
+UN_ZIP="jdk1.8.0_231"
+URL="http://qq008.cn/download/${FILE_ZIP}"
 
 if [ "$UID" != 0 ];then
         echo    "User must be root, Please try by root!"
@@ -19,21 +20,20 @@ if [ ! -e "$DIR" ];then
 fi
 
 cd $DIR
-
-if [ -e $FILE ];then
-        echo "$FILE file exist"
-        tar -zxvf $FILE
+if [ -e $FILE_ZIP ];then
+        echo "$FILE_ZIP file exist"
+        tar -zxvf $FILE_ZIP
 else
         wget $URL
-        tar -zxvf $FILE
+        tar -zxvf $FILE_ZIP
 fi
 
 sleep 1
 
 cat >> /etc/profile << EOF
 #java environment
-export JAVA_HOME=/usr/java/jdk1.8.0_231
-export PATH=$PATH:/usr/java/jdk1.8.0_231/bin
+export JAVA_HOME=/usr/java/${UN_ZIP}
+export PATH=$PATH:/usr/java/${UN_ZIP}/bin
 export CLASSPATH=.:${JAVA_HOME}/lib/dt.jar:${JAVA_HOME}/lib/tools.jar
 EOF
 
